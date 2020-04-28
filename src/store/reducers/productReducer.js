9c061f7e11e8c0b2569bef0f47b8fc97d249ed45
ibragimova01 <ibragimova.shaknoza@mail.ru>
2020-04-28 09:32:58 +0700

    feat(catalog): add catalog items

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -2,27 +2,123 @@ const initState = {
   products: [
     {
       id:1, 
-      title:"Розовое мыло",
-      price: 300,
-      image: "soap1.jpg"
+      title:"Мыло 'Морской бриз'",
+      price: 250,
+      image: "soap14.jpg"
     },
     {
       id:2, 
       title:"Мыло с кристаллами",
-      price: 400,
+      price: 199,
       image: "soap2.jpg"
     },
     {
       id:3, 
       title:"Декоративное мыло",
-      price: 250,
+      price: 150,
       image: "soap3.jpg"
     },
     {
       id:4, 
       title:"Йогуртовое мыло",
-      price: 422,
+      price: 299,
       image: "soap4.jpg"
+    },
+    {
+      id:5, 
+      title:"Мыло-скраб",
+      price: 220,
+      image: "soap5.jpg"
+    },
+    {
+      id:6, 
+      title:"Мыло с сухоцветами",
+      price: 250,
+      image: "soap6.jpg"
+    },
+    {
+      id:7, 
+      title:"Оливковое мыло",
+      price: 270,
+      image: "soap7.jpg"
+    },
+    {
+      id:8, 
+      title:"Лавандовое мыло",
+      price: 299,
+      image: "soap8.jpg"
+    },
+    {
+      id:9, 
+      title:"Розовое мыло",
+      price: 180,
+      image: "soap9.jpg"
+    },
+    {
+      id:10, 
+      title:"Мыло с пчелиным воском",
+      price: 299,
+      image: "soap10.jpg"
+    },
+    {
+      id:11, 
+      title: "Мыло 'Малиновые грезы'",
+      price: 299,
+      image: "soap11.jpg"
+    },
+    {
+      id:12, 
+      title:"Мыло из козьего молока",
+      price: 299,
+      image: "soap12.png"
+    },
+    {
+      id:13, 
+      title:"Брусничное мыло",
+      price: 280,
+      image: "soap13.jpg"
+    },
+    {
+      id:14, 
+      title:"Мыльное ассорти",
+      price: 200,
+      image: "soap1.jpg"
+    },
+    {
+      id:15, 
+      title:"Мыло-скраб с морской солью",
+      price: 270,
+      image: "soap17.jpg"
+    },
+    {
+      id:16, 
+      title:"Мыло 'Конфети'",
+      price: 250,
+      image: "soap21.jpg"
+    },
+    {
+      id:17, 
+      title:"Мыло с лепестками шиповника",
+      price: 299,
+      image: "soap15.jpg"
+    },
+    {
+      id:18, 
+      title:"Мыло 'Ягодный микс'",
+      price: 320,
+      image: "18.jpg"
+    },
+    {
+      id:19, 
+      title:"Крем-мыло с ванилью",
+      price: 250,
+      image: "19.jpg"
+    },
+    {
+      id:20, 
+      title:"Мыло 'Кусочек моря'",
+      price: 280,
+      image: "20.jpg"
     }
   ]
 }
